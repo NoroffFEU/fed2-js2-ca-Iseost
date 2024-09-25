9eeef09ed353b97bcd104f29a6e62bd3e9ea92d0
Iseost <iselin.osterhus@gmail.com>
2024-09-25 10:16:39 +0200

    Register and login
    The api for register and login is done.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ import "./css/style.css";
 
 import router from "./js/router";
 
+import "../auth/register";
+import "../auth/login";
+
 await router(window.location.pathname);
--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,28 @@
-export async function login({ email, password }) {}
+// export async function login({ email, password }) {}
+
+export async function login(email, password) {
+    const data = await fetch("https://v2.api.noroff.dev/auth/login", {
+        method: 'post',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify ({
+            email:email,
+            password:password
+        })
+    });
+
+    
+    if(data.status === 200){
+        const tech = await data.json();
+        const token = tech.data.accessToken;
+        const name = tech.data.name;
+        localStorage.setItem('accessToken', token);
+        localStorage.setItem('username', name);
+        window.location.replace('../post/index.html')
+    } else {
+        document.getElementById('error_message_login').textContent = 'Failed to login. Email address or password is wrong. Please try again'
+    }
+
+}
